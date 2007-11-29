7e7e8d9efeda3ee8b9931f44fdea1706ffd54397
ashuang <ashuang@ac3b5936-af81-11dd-b4f7-7faeb9b304ab>
2007-11-29 19:23:37 +0000

    filter_jpeg: add default huffman tables when decompressing images missing huffman tables

--- trunk/libcam/filter_jpeg.c
+++ trunk/libcam/filter_jpeg.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <jpeglib.h>
+#include <jerror.h>
 
 #include "filter_jpeg.h"
+#include "pixels.h"
 #include "dbg.h"
 
 #include "plugin.h"
@@ -13,14 +16,15 @@
 #define FILTER_JPEG_LABEL "JPEG"
 #endif
 
-static int jpeg_decompress_to_8u_rgb (const uint8_t * src, int src_size,
+static int _jpeg_decompress_to_8u_rgb (const uint8_t * src, int src_size,
         uint8_t * dest, int width, int height, int stride);
-static int jpeg_compress_8u_gray (const uint8_t * src, int width, int height, int stride,
-        uint8_t * dest, int * destsize, int quality);
-static int jpeg_compress_8u_rgb (const uint8_t * src, int width, int height, int stride,
-        uint8_t * dest, int * destsize, int quality);
-static int jpeg_compress_8u_bgra (const uint8_t * src, int width, int height, int stride,
-        uint8_t * dest, int * destsize, int quality);
+static int _jpeg_compress_8u_gray (const uint8_t * src, int width, int height, 
+        int stride, uint8_t * dest, int * destsize, int quality);
+static int _jpeg_compress_8u_rgb (const uint8_t * src, int width, int height, 
+        int stride, uint8_t * dest, int * destsize, int quality);
+static int _jpeg_compress_8u_bgra (const uint8_t * src, int width, int height, 
+        int stride, uint8_t * dest, int * destsize, int quality);
+static void _jpeg_std_huff_tables (j_decompress_ptr cinfo);
 
 CamUnitDriver *
 cam_filter_jpeg_driver_new()
@@ -57,7 +61,6 @@ cam_plugin_create (GTypeModule * module)
             (CamUnitConstructor)cam_filter_jpeg_new, module);
 }
 #endif
-
 
 static void
 cam_filter_jpeg_init (CamFilterJpeg *self)
@@ -103,16 +106,16 @@ on_input_frame_ready (CamUnit *super, const CamFrameBuffer *inbuf,
     CamFilterJpeg * self = CAM_FILTER_JPEG (super);
     int quality = cam_unit_control_get_int (self->quality_control);
     if (infmt->pixelformat == CAM_PIXEL_FORMAT_RGB)
-        jpeg_compress_8u_rgb (inbuf->data, width, height, infmt->row_stride,
+        _jpeg_compress_8u_rgb (inbuf->data, width, height, infmt->row_stride,
                 outbuf->data, &outsize, quality);
     else if (infmt->pixelformat == CAM_PIXEL_FORMAT_BGRA)
-        jpeg_compress_8u_bgra (inbuf->data, width, height, infmt->row_stride,
+        _jpeg_compress_8u_bgra (inbuf->data, width, height, infmt->row_stride,
                 outbuf->data, &outsize, quality);
     else if (infmt->pixelformat == CAM_PIXEL_FORMAT_GRAY)
-        jpeg_compress_8u_gray (inbuf->data, width, height, infmt->row_stride,
+        _jpeg_compress_8u_gray (inbuf->data, width, height, infmt->row_stride,
                 outbuf->data, &outsize, quality);
     else if (infmt->pixelformat == CAM_PIXEL_FORMAT_MJPEG) {
-        jpeg_decompress_to_8u_rgb (inbuf->data, inbuf->bytesused,
+        _jpeg_decompress_to_8u_rgb (inbuf->data, inbuf->bytesused,
                 outbuf->data, infmt->width, infmt->height, outfmt->row_stride);
         outsize = outfmt->row_stride * infmt->height;
     }
@@ -152,14 +155,6 @@ on_input_format_changed (CamUnit *super, const CamUnitFormat *infmt)
 
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <jpeglib.h>
-#include <jerror.h>
-
-#include "pixels.h"
-
 static void
 init_source (j_decompress_ptr cinfo)
 {
@@ -185,7 +180,7 @@ term_source (j_decompress_ptr cinfo)
 }
 
 static int
-jpeg_decompress_to_8u_rgb (const uint8_t * src, int src_size,
+_jpeg_decompress_to_8u_rgb (const uint8_t * src, int src_size,
         uint8_t * dest, int width, int height, int stride)
 {
     struct jpeg_decompress_struct cinfo;
@@ -206,6 +201,12 @@ jpeg_decompress_to_8u_rgb (const uint8_t * src, int src_size,
 
     jpeg_read_header (&cinfo, TRUE);
     cinfo.out_color_space = JCS_RGB;
+
+    if (! (cinfo.dc_huff_tbl_ptrs[0] || cinfo.dc_huff_tbl_ptrs[1] ||
+           cinfo.ac_huff_tbl_ptrs[0] || cinfo.ac_huff_tbl_ptrs[1])) {
+        _jpeg_std_huff_tables(&cinfo);
+    }
+
     jpeg_start_decompress (&cinfo);
 
     if (cinfo.output_height != height || cinfo.output_width != width) {
@@ -243,7 +244,8 @@ term_destination (j_compress_ptr cinfo)
     /* do nothing */
 }
 
-static int jpeg_compress_8u_gray (const uint8_t * src, int width, int height, int stride,
+static int 
+_jpeg_compress_8u_gray (const uint8_t * src, int width, int height, int stride,
         uint8_t * dest, int * destsize, int quality)
 {
     struct jpeg_compress_struct cinfo;
@@ -278,7 +280,8 @@ static int jpeg_compress_8u_gray (const uint8_t * src, int width, int height, in
     return 0;
 }
 
-static int jpeg_compress_8u_rgb (const uint8_t * src, int width, int height, int stride,
+static int 
+_jpeg_compress_8u_rgb (const uint8_t * src, int width, int height, int stride,
         uint8_t * dest, int * destsize, int quality)
 {
     struct jpeg_compress_struct cinfo;
@@ -313,7 +316,8 @@ static int jpeg_compress_8u_rgb (const uint8_t * src, int width, int height, int
     return 0;
 }
 
-static int jpeg_compress_8u_bgra (const uint8_t * src, int width, int height, int stride,
+static int 
+_jpeg_compress_8u_bgra (const uint8_t * src, int width, int height, int stride,
         uint8_t * dest, int * destsize, int quality)
 {
     struct jpeg_compress_struct cinfo;
@@ -350,3 +354,117 @@ static int jpeg_compress_8u_bgra (const uint8_t * src, int width, int height, in
     jpeg_destroy_compress (&cinfo);
     return 0;
 }
+
+
+// ============= BEGIN code taken from libjpeg ===============
+
+/*
+ * Huffman table setup routines
+ */
+
+static void
+add_huff_table (j_decompress_ptr cinfo,
+		JHUFF_TBL **htblptr, const uint8_t *bits, const uint8_t *val)
+/* Define a Huffman table */
+{
+    int nsymbols, len;
+
+    if (*htblptr == NULL)
+        *htblptr = jpeg_alloc_huff_table((j_common_ptr) cinfo);
+
+    /* Copy the number-of-symbols-of-each-code-length counts */
+    memcpy((*htblptr)->bits, bits, sizeof((*htblptr)->bits));
+
+    /* Validate the counts.  We do this here mainly so we can copy the right
+     * number of symbols from the val[] array, without risking marching off
+     * the end of memory.  jchuff.c will do a more thorough test later.
+     */
+    nsymbols = 0;
+    for (len = 1; len <= 16; len++)
+        nsymbols += bits[len];
+    if (nsymbols < 1 || nsymbols > 256)
+        ERREXIT(cinfo, JERR_BAD_HUFF_TABLE);
+
+    memcpy((*htblptr)->huffval, val, nsymbols * sizeof(uint8_t));
+
+    /* Initialize sent_table FALSE so table will be written to JPEG file. */
+    (*htblptr)->sent_table = FALSE;
+}
+
+
+static void
+_jpeg_std_huff_tables (j_decompress_ptr cinfo)
+/* Set up the standard Huffman tables (cf. JPEG standard section K.3) */
+/* IMPORTANT: these are only valid for 8-bit data precision! */
+{
+    static const uint8_t bits_dc_luminance[17] =
+    { /* 0-base */ 0, 0, 1, 5, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 };
+    static const uint8_t val_dc_luminance[] =
+    { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+    static const uint8_t bits_dc_chrominance[17] =
+    { /* 0-base */ 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 };
+    static const uint8_t val_dc_chrominance[] =
+    { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+    static const uint8_t bits_ac_luminance[17] =
+    { /* 0-base */ 0, 0, 2, 1, 3, 3, 2, 4, 3, 5, 5, 4, 4, 0, 0, 1, 0x7d };
+    static const uint8_t val_ac_luminance[] =
+    { 0x01, 0x02, 0x03, 0x00, 0x04, 0x11, 0x05, 0x12,
+        0x21, 0x31, 0x41, 0x06, 0x13, 0x51, 0x61, 0x07,
+        0x22, 0x71, 0x14, 0x32, 0x81, 0x91, 0xa1, 0x08,
+        0x23, 0x42, 0xb1, 0xc1, 0x15, 0x52, 0xd1, 0xf0,
+        0x24, 0x33, 0x62, 0x72, 0x82, 0x09, 0x0a, 0x16,
+        0x17, 0x18, 0x19, 0x1a, 0x25, 0x26, 0x27, 0x28,
+        0x29, 0x2a, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39,
+        0x3a, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49,
+        0x4a, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59,
+        0x5a, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69,
+        0x6a, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79,
+        0x7a, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89,
+        0x8a, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98,
+        0x99, 0x9a, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7,
+        0xa8, 0xa9, 0xaa, 0xb2, 0xb3, 0xb4, 0xb5, 0xb6,
+        0xb7, 0xb8, 0xb9, 0xba, 0xc2, 0xc3, 0xc4, 0xc5,
+        0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xd2, 0xd3, 0xd4,
+        0xd5, 0xd6, 0xd7, 0xd8, 0xd9, 0xda, 0xe1, 0xe2,
+        0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9, 0xea,
+        0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8,
+        0xf9, 0xfa };
+
+    static const uint8_t bits_ac_chrominance[17] =
+    { /* 0-base */ 0, 0, 2, 1, 2, 4, 4, 3, 4, 7, 5, 4, 4, 0, 1, 2, 0x77 };
+    static const uint8_t val_ac_chrominance[] =
+    { 0x00, 0x01, 0x02, 0x03, 0x11, 0x04, 0x05, 0x21,
+        0x31, 0x06, 0x12, 0x41, 0x51, 0x07, 0x61, 0x71,
+        0x13, 0x22, 0x32, 0x81, 0x08, 0x14, 0x42, 0x91,
+        0xa1, 0xb1, 0xc1, 0x09, 0x23, 0x33, 0x52, 0xf0,
+        0x15, 0x62, 0x72, 0xd1, 0x0a, 0x16, 0x24, 0x34,
+        0xe1, 0x25, 0xf1, 0x17, 0x18, 0x19, 0x1a, 0x26,
+        0x27, 0x28, 0x29, 0x2a, 0x35, 0x36, 0x37, 0x38,
+        0x39, 0x3a, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48,
+        0x49, 0x4a, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58,
+        0x59, 0x5a, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68,
+        0x69, 0x6a, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78,
+        0x79, 0x7a, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87,
+        0x88, 0x89, 0x8a, 0x92, 0x93, 0x94, 0x95, 0x96,
+        0x97, 0x98, 0x99, 0x9a, 0xa2, 0xa3, 0xa4, 0xa5,
+        0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xb2, 0xb3, 0xb4,
+        0xb5, 0xb6, 0xb7, 0xb8, 0xb9, 0xba, 0xc2, 0xc3,
+        0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xd2,
+        0xd3, 0xd4, 0xd5, 0xd6, 0xd7, 0xd8, 0xd9, 0xda,
+        0xe2, 0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9,
+        0xea, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8,
+        0xf9, 0xfa };
+
+    add_huff_table(cinfo, &cinfo->dc_huff_tbl_ptrs[0],
+            bits_dc_luminance, val_dc_luminance);
+    add_huff_table(cinfo, &cinfo->ac_huff_tbl_ptrs[0],
+            bits_ac_luminance, val_ac_luminance);
+    add_huff_table(cinfo, &cinfo->dc_huff_tbl_ptrs[1],
+            bits_dc_chrominance, val_dc_chrominance);
+    add_huff_table(cinfo, &cinfo->ac_huff_tbl_ptrs[1],
+            bits_ac_chrominance, val_ac_chrominance);
+}
+
+// ============= END code taken from libjpeg =============
