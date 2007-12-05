43084377b44534eb805d00f37faaa812988c6e2a
david.moore <david.moore@ac3b5936-af81-11dd-b4f7-7faeb9b304ab>
2007-12-05 23:47:35 +0000

    Detect SSE2 and SSE3 at runtime and act accordingly

--- trunk/libcam/pixels.h
+++ trunk/libcam/pixels.h
@@ -9,6 +9,10 @@
     ((uint32_t)(c)<<16) | \
     ((uint32_t)(d)<<24))
 
+#define IS_ALIGNED16(x) (((uintptr_t)(x) & 0xf) == 0)
+#define IS_ALIGNED32(x) (((uintptr_t)(x) & 0x1f) == 0)
+#define IS_ALIGNED128(x) (((uintptr_t)(x) & 0x7f) == 0)
+
 #ifdef __cplusplus
 extern "C" {
 #endif
