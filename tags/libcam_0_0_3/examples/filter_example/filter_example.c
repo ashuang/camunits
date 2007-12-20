d2aab5f323d7c598b055d9e1158d6f04777cf585
david.moore <david.moore@ac3b5936-af81-11dd-b4f7-7faeb9b304ab>
2007-12-20 06:28:11 +0000

    Release 0.0.3

---
+++ tags/libcam_0_0_3/examples/filter_example/filter_example.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <libcam/cam.h>
+
+#include "filter_example.h"
+
+/* Boilerplate */
+G_DEFINE_TYPE (MyFilterExample, my_filter_example, CAM_TYPE_UNIT);
+
+CamUnitDriver *
+my_filter_example_driver_new()
+{
+    return cam_unit_driver_new_stock ("filter", "example",
+            "Example", 0, (CamUnitConstructor)my_filter_example_new);
+}
+
+// ============== MyFilterExample ===============
+static void my_filter_example_finalize (GObject *obj);
+static void on_input_frame_ready (CamUnit * super, const CamFrameBuffer *inbuf,
+        const CamUnitFormat *infmt);
+static void on_input_format_changed (CamUnit *super, 
+        const CamUnitFormat *infmt);
+
+// Class initializer
+static void
+my_filter_example_class_init (MyFilterExampleClass *klass)
+{
+    // override the destructor
+    G_OBJECT_CLASS (klass)->finalize = my_filter_example_finalize;
+
+    // override the "on_input_frame_ready" method
+    klass->parent_class.on_input_frame_ready = on_input_frame_ready;
+}
+
+// First part of the constructor
+MyFilterExample * 
+my_filter_example_new()
+{
+    return MY_FILTER_EXAMPLE (g_object_new(MY_TYPE_FILTER_EXAMPLE, NULL));
+}
+
+// Initializer.  This is the second part of the constructor.
+static void
+my_filter_example_init (MyFilterExample *self)
+{
+    // Initialize the unit with some reasonable defaults here.
+    CamUnit *super = CAM_UNIT (self);
+
+    // create a control
+    self->patch_intensity_control = cam_unit_add_control_int (super, 
+            "patch-intensity", "Patch Intensity", 0, 255, 1, 127, 1);
+
+    // request notification when the input of the unit changes
+    g_signal_connect (G_OBJECT(self), "input-format-changed",
+            G_CALLBACK(on_input_format_changed), NULL);
+}
+
+// destructor.
+static void
+my_filter_example_finalize (GObject *obj)
+{
+    // If we allocated memory on the heap/freestore, we'd release it here
+
+    // invoke the superclass destructor
+    G_OBJECT_CLASS (my_filter_example_parent_class)->finalize(obj);
+}
+
+// this method is called whenever the input unit produces a frame
+static void
+on_input_frame_ready (CamUnit *super, const CamFrameBuffer *inbuf, 
+        const CamUnitFormat *infmt)
+{
+    MyFilterExample *self = MY_FILTER_EXAMPLE(super);
+
+    CamFrameBuffer *outbuf = 
+        cam_framebuffer_new_alloc (super->fmt->max_data_size);
+
+    cam_pixel_copy_8u_generic (inbuf->data, infmt->row_stride,
+            outbuf->data, super->fmt->row_stride,
+            0, 0, 0, 0, infmt->width, infmt->height, 
+            cam_pixel_format_bpp (infmt->pixelformat));
+
+    // draw a little rectangle
+    int x0 = super->fmt->width / 4;
+    int x1 = x0 * 3;
+    int rw = x1 - x0;
+    int y0 = super->fmt->height / 4;
+    int y1 = y0 * 3;
+
+    int val = cam_unit_control_get_int (self->patch_intensity_control);
+
+    int i;
+    for (i=y0; i<y1; i++) {
+        memset (outbuf->data + i*super->fmt->row_stride + x0*3, val, rw*3);
+    }
+
+    // copy the timestamp and metadata dictionary
+    cam_framebuffer_copy_metadata (outbuf, inbuf);
+
+    outbuf->bytesused = super->fmt->row_stride * super->fmt->height;
+
+    cam_unit_produce_frame (super, outbuf, super->fmt);
+    g_object_unref (outbuf);
+}
+
+// this is the signal handler attached in "my_filter_example_init", and is
+// called when the format of the input data changes.
+static void
+on_input_format_changed (CamUnit *super, const CamUnitFormat *infmt)
+{
+    // first, clear all available output formats from this unit
+    cam_unit_remove_all_output_formats (super);
+
+    // If there is no input, then we can't produce output.  
+    if (!infmt) return;
+   
+    // actually, we can only handle 8-bit RGB input data.
+    if (infmt->pixelformat != CAM_PIXEL_FORMAT_RGB) return;
+
+    cam_unit_add_output_format_full (super, infmt->pixelformat,
+            infmt->name, infmt->width, infmt->height, 
+            infmt->row_stride, infmt->max_data_size);
+}
---
+++ tags/libcam_0_0_3/libcam-gtk/unit_description_widget.h
@@ -0,0 +1,49 @@
+#ifndef __cam_unit_description_widget_h__
+#define __cam_unit_description_widget_h__
+
+#include <gtk/gtk.h>
+
+#include <libcam/unit.h>
+#include "unit_manager_widget.h"
+
+
+G_BEGIN_DECLS
+
+#define CAM_TYPE_UNIT_DESCRIPTION_WIDGET  cam_unit_description_widget_get_type()
+#define CAM_UNIT_DESCRIPTION_WIDGET(obj)  \
+    (G_TYPE_CHECK_INSTANCE_CAST((obj), \
+    CAM_TYPE_UNIT_DESCRIPTION_WIDGET, CamUnitDescriptionWidget))
+#define CAM_UNIT_DESCRIPTION_WIDGET_CLASS(klass) \
+    (G_TYPE_CHECK_CLASS_CAST ((klass), \
+    CAM_TYPE_UNIT_DESCRIPTION_WIDGET, CamUnitDescriptionWidgetClass ))
+#define IS_CAM_UNIT_DESCRIPTION_WIDGET(obj)   \
+    (G_TYPE_CHECK_INSTANCE_TYPE ((obj), CAM_TYPE_UNIT_DESCRIPTION_WIDGET ))
+#define IS_CAM_UNIT_DESCRIPTION_WIDGET_CLASS(klass)   \
+    (G_TYPE_CHECK_CLASS_TYPE( (klass), CAM_TYPE_UNIT_DESCRIPTION_WIDGET))
+
+typedef struct _CamUnitDescriptionWidget CamUnitDescriptionWidget;
+typedef struct _CamUnitDescriptionWidgetClass CamUnitDescriptionWidgetClass;
+
+struct _CamUnitDescriptionWidget
+{
+    GtkTextView parent;
+
+    /*< private >*/
+    CamUnitManagerWidget *manager;
+};
+
+struct _CamUnitDescriptionWidgetClass
+{
+    GtkTextViewClass parent_class;
+};
+
+GType cam_unit_description_widget_get_type(void);
+
+CamUnitDescriptionWidget *cam_unit_description_widget_new (void);
+
+int cam_unit_description_widget_set_manager( CamUnitDescriptionWidget* self, 
+        CamUnitManagerWidget *manager );
+
+G_END_DECLS
+
+#endif  /* __cam_unit_description_widget_h__ */
