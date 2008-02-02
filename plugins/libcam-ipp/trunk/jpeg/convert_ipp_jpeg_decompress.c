3ec735f8774af6cb3d2f5d75b4d0341189deb269
ashuang <ashuang@ac3b5936-af81-11dd-b4f7-7faeb9b304ab>
2008-02-02 01:25:30 +0000

    split IPP JPEG unit into two (compress/decompress)

--- plugins/libcam-ipp/trunk/jpeg/convert_ipp_jpeg_decompress.c
+++ plugins/libcam-ipp/trunk/jpeg/convert_ipp_jpeg_decompress.c
@@ -6,29 +6,25 @@
 #include "jpeglib.h"
 #include "jerror.h"
 
-#include "convert_ipp_jpeg.h"
-
 #include <libcam/plugin.h>
+
+#include "convert_ipp_jpeg_decompress.h"
 
 #define err(args...) fprintf(stderr, args)
 
 static int _jpegipp_decompress_to_8u_rgb (const uint8_t * src, int src_size,
         uint8_t * dest, int width, int height, int stride);
-static int _jpegipp_compress_8u_gray (const uint8_t * src, int width, int height, 
-        int stride, uint8_t * dest, int * destsize, int quality);
-static int _jpegipp_compress_8u_rgb (const uint8_t * src, int width, int height, 
-        int stride, uint8_t * dest, int * destsize, int quality);
-static int _jpegipp_compress_8u_bgra (const uint8_t * src, int width, int height, 
-        int stride, uint8_t * dest, int * destsize, int quality);
 static void _jpegipp_std_huff_tables (j_decompress_ptr cinfo);
 
-// ============== CamippJpeg ===============
+// ============== CamippJpegDecompress ===============
 static void on_input_frame_ready (CamUnit * super, const CamFrameBuffer *inbuf,
         const CamUnitFormat *infmt);
 static void on_input_format_changed (CamUnit *super, 
         const CamUnitFormat *infmt);
+static int _stream_init (CamUnit * super, const CamUnitFormat * format);
+static int _stream_shutdown (CamUnit * super);
 
-CAM_PLUGIN_TYPE (CamippJpeg, camipp_jpeg, CAM_TYPE_UNIT);
+CAM_PLUGIN_TYPE (CamippJpegDecompress, camipp_jpeg_decompress, CAM_TYPE_UNIT);
 
 void cam_plugin_initialize (GTypeModule * module);
 CamUnitDriver * cam_plugin_create (GTypeModule * module);
@@ -38,104 +34,87 @@ CamUnitDriver * cam_plugin_create (GTypeModule * module);
 void
 cam_plugin_initialize (GTypeModule * module)
 {
-    camipp_jpeg_register_type (module);
+    camipp_jpeg_decompress_register_type (module);
 }
 
 CamUnitDriver *
 cam_plugin_create (GTypeModule * module)
 {
-    return cam_unit_driver_new_stock_full ("convert.ipp", "jpeg",
-            "IPP JPEG", 0, (CamUnitConstructor)camipp_jpeg_new, module);
+    return cam_unit_driver_new_stock_full ("convert.ipp", "jpeg_decompress",
+            "IPP JPEG Decompress", 0, 
+            (CamUnitConstructor)camipp_jpeg_decompress_new, module);
 }
 
 static void
-camipp_jpeg_init (CamippJpeg *self)
+camipp_jpeg_decompress_init (CamippJpegDecompress *self)
 {
     // constructor.  Initialize the unit with some reasonable defaults here.
-    CamUnit *super = CAM_UNIT (self);
-
-    self->quality_control = cam_unit_add_control_int (super, "quality", 
-            "Quality", 1, 100, 1, 94, 1);
+    self->outbuf = NULL;
     g_signal_connect (G_OBJECT(self), "input-format-changed",
             G_CALLBACK(on_input_format_changed), NULL);
 }
 
 static void
-camipp_jpeg_class_init (CamippJpegClass *klass)
+camipp_jpeg_decompress_class_init (CamippJpegDecompressClass *klass)
 {
     klass->parent_class.on_input_frame_ready = on_input_frame_ready;
+    klass->parent_class.stream_init = _stream_init;
+    klass->parent_class.stream_shutdown = _stream_shutdown;
 }
 
-CamippJpeg * 
-camipp_jpeg_new()
+CamippJpegDecompress * 
+camipp_jpeg_decompress_new()
 {
-    return CAMIPP_JPEG(
-            g_object_new(CAMIPP_TYPE_JPEG, NULL));
+    return CAMIPP_JPEG_DECOMPRESS(
+            g_object_new(CAMIPP_TYPE_JPEG_DECOMPRESS, NULL));
+}
+
+static int 
+_stream_init (CamUnit * super, const CamUnitFormat * format)
+{
+    CamippJpegDecompress *self = CAMIPP_JPEG_DECOMPRESS (super);
+    self->outbuf = cam_framebuffer_new_alloc (format->max_data_size);
+    return 0;
+}
+
+static int 
+_stream_shutdown (CamUnit * super)
+{
+    g_object_unref (CAMIPP_JPEG_DECOMPRESS (super)->outbuf);
+    CAMIPP_JPEG_DECOMPRESS (super)->outbuf = NULL;
+    return 0;
 }
 
 static void 
 on_input_frame_ready (CamUnit *super, const CamFrameBuffer *inbuf,
         const CamUnitFormat *infmt)
 {
-    CamFrameBuffer *outbuf = 
-        cam_framebuffer_new_alloc (super->fmt->max_data_size);;
-
+    CamippJpegDecompress *self = CAMIPP_JPEG_DECOMPRESS (super);
     const CamUnitFormat *outfmt = cam_unit_get_output_format(super);
 
-    int width = infmt->width;
-    int height = infmt->height;
-    int outsize = outbuf->length;
-    CamippJpeg * self = CAMIPP_JPEG (super);
-    int quality = cam_unit_control_get_int (self->quality_control);
-    if (infmt->pixelformat == CAM_PIXEL_FORMAT_RGB)
-        _jpegipp_compress_8u_rgb (inbuf->data, width, height, infmt->row_stride,
-                outbuf->data, &outsize, quality);
-    else if (infmt->pixelformat == CAM_PIXEL_FORMAT_BGRA)
-        _jpegipp_compress_8u_bgra (inbuf->data, width, height, infmt->row_stride,
-                outbuf->data, &outsize, quality);
-    else if (infmt->pixelformat == CAM_PIXEL_FORMAT_GRAY)
-        _jpegipp_compress_8u_gray (inbuf->data, width, height, infmt->row_stride,
-                outbuf->data, &outsize, quality);
-    else if (infmt->pixelformat == CAM_PIXEL_FORMAT_MJPEG) {
+    if (outfmt->pixelformat == CAM_PIXEL_FORMAT_RGB) {
         _jpegipp_decompress_to_8u_rgb (inbuf->data, inbuf->bytesused,
-                outbuf->data, infmt->width, infmt->height, outfmt->row_stride);
-        outsize = outfmt->row_stride * infmt->height;
+                self->outbuf->data, infmt->width, infmt->height, 
+                outfmt->row_stride);
+        self->outbuf->bytesused = outfmt->row_stride * infmt->height;
     }
+    cam_framebuffer_copy_metadata (self->outbuf, inbuf);
 
-    cam_framebuffer_copy_metadata(outbuf, inbuf);
-    outbuf->bytesused = outsize;
-
-    cam_unit_produce_frame (super, outbuf, outfmt);
-    g_object_unref (outbuf);
+    cam_unit_produce_frame (super, self->outbuf, outfmt);
 }
 
 static void
 on_input_format_changed (CamUnit *super, const CamUnitFormat *infmt)
 {
     cam_unit_remove_all_output_formats (super);
+    if (!infmt || infmt->pixelformat != CAM_PIXEL_FORMAT_MJPEG) return;
 
-    if (!infmt) return;
-
-    if (! (infmt->pixelformat == CAM_PIXEL_FORMAT_GRAY ||
-           infmt->pixelformat == CAM_PIXEL_FORMAT_RGB ||
-           infmt->pixelformat == CAM_PIXEL_FORMAT_BGRA ||
-           infmt->pixelformat == CAM_PIXEL_FORMAT_MJPEG)) return;
-
-    if (infmt->pixelformat == CAM_PIXEL_FORMAT_MJPEG) {
-        int stride = infmt->width * 3;
-        int max_data_size = stride * infmt->height;
-        cam_unit_add_output_format_full (super, CAM_PIXEL_FORMAT_RGB,
-                NULL, infmt->width, infmt->height, 
-                stride, max_data_size);
-    } else {
-        int max_data_size = infmt->width * 3 * infmt->height;
-        cam_unit_add_output_format_full (super, CAM_PIXEL_FORMAT_MJPEG,
-                NULL, infmt->width, infmt->height,
-                0, max_data_size);
-    }
+    int stride = infmt->width * 3;
+    int max_data_size = stride * infmt->height;
+    cam_unit_add_output_format_full (super, CAM_PIXEL_FORMAT_RGB,
+            NULL, infmt->width, infmt->height, 
+            stride, max_data_size);
 }
-
-
 
 static void
 init_source (j_decompress_ptr cinfo)
@@ -228,137 +207,6 @@ _jpegipp_decompress_to_8u_rgb (const uint8_t * src, int src_size,
     jpegipp_destroy_decompress (&cinfo);
     return 0;
 }
-
-static void
-init_destination (j_compress_ptr cinfo)
-{
-    /* do nothing */
-}
-
-static boolean
-empty_output_buffer (j_compress_ptr cinfo)
-{
-    fprintf (stderr, "Error: JPEG compressor ran out of buffer space\n");
-    return TRUE;
-}
-
-static void
-term_destination (j_compress_ptr cinfo)
-{
-    /* do nothing */
-}
-
-static int 
-_jpegipp_compress_8u_gray (const uint8_t * src, int width, int height, int stride,
-        uint8_t * dest, int * destsize, int quality)
-{
-    struct jpegipp_compress_struct cinfo;
-    struct jpegipp_error_mgr jerr;
-    struct jpegipp_destination_mgr jdest;
-    int out_size = *destsize;
-
-    cinfo.err = jpegipp_std_error (&jerr);
-    jpegipp_create_compress (&cinfo);
-    jdest.next_output_byte = dest;
-    jdest.free_in_buffer = out_size;
-    jdest.init_destination = init_destination;
-    jdest.empty_output_buffer = empty_output_buffer;
-    jdest.term_destination = term_destination;
-    cinfo.dest = &jdest;
-
-    cinfo.image_width = width;
-    cinfo.image_height = height;
-    cinfo.input_components = 1;
-    cinfo.in_color_space = JCS_GRAYSCALE;
-    jpegipp_set_defaults (&cinfo);
-    jpegipp_set_quality (&cinfo, quality, TRUE);
-
-    jpegipp_start_compress (&cinfo, TRUE);
-    while (cinfo.next_scanline < height) {
-        JSAMPROW row = (JSAMPROW)(src + cinfo.next_scanline * stride);
-        jpegipp_write_scanlines (&cinfo, &row, 1);
-    }
-    jpegipp_finish_compress (&cinfo);
-    *destsize = out_size - jdest.free_in_buffer;
-    jpegipp_destroy_compress (&cinfo);
-    return 0;
-}
-
-static int 
-_jpegipp_compress_8u_rgb (const uint8_t * src, int width, int height, int stride,
-        uint8_t * dest, int * destsize, int quality)
-{
-    struct jpegipp_compress_struct cinfo;
-    struct jpegipp_error_mgr jerr;
-    struct jpegipp_destination_mgr jdest;
-    int out_size = *destsize;
-
-    cinfo.err = jpegipp_std_error (&jerr);
-    jpegipp_create_compress (&cinfo);
-    jdest.next_output_byte = dest;
-    jdest.free_in_buffer = out_size;
-    jdest.init_destination = init_destination;
-    jdest.empty_output_buffer = empty_output_buffer;
-    jdest.term_destination = term_destination;
-    cinfo.dest = &jdest;
-
-    cinfo.image_width = width;
-    cinfo.image_height = height;
-    cinfo.input_components = 3;
-    cinfo.in_color_space = JCS_RGB;
-    jpegipp_set_defaults (&cinfo);
-    jpegipp_set_quality (&cinfo, quality, TRUE);
-
-    jpegipp_start_compress (&cinfo, TRUE);
-    while (cinfo.next_scanline < height) {
-        JSAMPROW row = (JSAMPROW)(src + cinfo.next_scanline * stride);
-        jpegipp_write_scanlines (&cinfo, &row, 1);
-    }
-    jpegipp_finish_compress (&cinfo);
-    *destsize = out_size - jdest.free_in_buffer;
-    jpegipp_destroy_compress (&cinfo);
-    return 0;
-}
-
-static int 
-_jpegipp_compress_8u_bgra (const uint8_t * src, int width, int height, int stride,
-        uint8_t * dest, int * destsize, int quality)
-{
-    struct jpegipp_compress_struct cinfo;
-    struct jpegipp_error_mgr jerr;
-    struct jpegipp_destination_mgr jdest;
-    int out_size = *destsize;
-
-    cinfo.err = jpegipp_std_error (&jerr);
-    jpegipp_create_compress (&cinfo);
-    jdest.next_output_byte = dest;
-    jdest.free_in_buffer = out_size;
-    jdest.init_destination = init_destination;
-    jdest.empty_output_buffer = empty_output_buffer;
-    jdest.term_destination = term_destination;
-    cinfo.dest = &jdest;
-
-    cinfo.image_width = width;
-    cinfo.image_height = height;
-    cinfo.input_components = 3;
-    cinfo.in_color_space = JCS_RGB;
-    jpegipp_set_defaults (&cinfo);
-    jpegipp_set_quality (&cinfo, quality, TRUE);
-
-    jpegipp_start_compress (&cinfo, TRUE);
-    while (cinfo.next_scanline < height) {
-        uint8_t buf[width*3];
-        cam_pixel_convert_8u_bgra_to_8u_rgb (buf, 0, width, 1,
-                src + cinfo.next_scanline * stride, 0);
-        JSAMPROW row = (JSAMPROW) buf;
-        jpegipp_write_scanlines (&cinfo, &row, 1);
-    }
-    jpegipp_finish_compress (&cinfo);
-    *destsize = out_size - jdest.free_in_buffer;
-    jpegipp_destroy_compress (&cinfo);
-    return 0;
-}
-
 
 // ============= BEGIN code taken from libjpeg ===============
 
